d8f88697fc66cb622a6efbd8a5891c8169769b4b
David THENON <sveetch@gmail.com>
2017-02-20 02:12:40 +0100

    Enable 'wagtailstyleguide' in dev settings only

--- wagtaildemo/settings/dev.py
+++ wagtaildemo/settings/dev.py
@@ -7,6 +7,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # BASE_URL required for notification emails
 BASE_URL = 'http://localhost:8000'
 
+
+INSTALLED_APPS = tuple(list(INSTALLED_APPS)+[
+    'wagtail.contrib.wagtailstyleguide',
+])
+
+
 try:
     from .local import *
 except ImportError:
